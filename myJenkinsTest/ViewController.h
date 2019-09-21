edf3dabd90defb66d1140ecbd75646e94fba79ec
wulang150 <475750165@qq.com>
2019-09-21 14:11:05 +0800

    add project

---
+++ myJenkinsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myJenkinsTest
+//
+//  Created by Anker on 2019/9/21.
+//  Copyright © 2019 Anker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ myJenkinsTest/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  myJenkinsTest
+//
+//  Created by Anker on 2019/9/21.
+//  Copyright © 2019 Anker. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    UILabel *lab = [[UILabel alloc] initWithFrame:CGRectMake(30, 100, 120, 32)];
+    lab.layer.borderWidth = 1;
+    lab.text = @"sfdsfdsfds";
+    [self.view addSubview:lab];
+}
+
+
+@end
